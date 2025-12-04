a34b094de8fbad1fce29fb62154dc5188d84cad3
Vladim72m <vladim72dx@tutamail.com>
2025-12-04 23:21:00 +0300

    terms.py

--- terms.py
+++ terms.py
@@ -1,12 +1,19 @@
-print('Введите число')
-n = int(input())
-def operation(a, n, k):
-    if n:
-        for i in range(n+1, 0, -1): # -1 - длина шага 
-            if i <= k:
-                operation(a+[i], n-i, i)
-    else:
-        print( a, end=' , ')
-print('operation(',n,')=')  
-operation([], n, n)
+def partitions(n, min = 1): # создаем функцию 
+    if n == 0:
+        return [[]]  # возвращаем пустое разбиение
+    list = []
+    for i in range(min, n + 1):  
+        for j in partitions(n - i, i):  
+            list.append([i] + j)  # Добавляем  число к каждому разбиению
+    return list
 
+def b(c): # создаем функцию
+    parts = partitions(c) # список всех разложений
+    for a in parts:
+        print(a)
+        
+def main():
+    c = int(input("Введите число, которое хотите разложить на сумму целых чисел: "))
+    print(b(c)) 
+if __name__ == "__main__":
+    main()
