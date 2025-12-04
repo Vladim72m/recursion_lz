aa5e72db3499c7bbfa6c691029bc0370838115b9
Vladim72m <vladim72dx@tutamail.com>
2025-12-04 15:47:01 +0300

    Euclid.py

--- Euclid.py
+++ Euclid.py
@@ -4,8 +4,9 @@ def gcd(a,b):
     else:
         return gcd(b,a%b)
 def main():
-    a=int(input('Введите первое4 число:'))
+    a=int(input('Введите первое число:'))
     b=int (input('Введите второе число:'))
     print('НОД:',gcd(a,b))
 if __name__ == "__main__":
+
     main()
