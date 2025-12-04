13ff88579fbfafccdb720a8ef71d73dae5bb4a03
Vladim72m <vladim72dx@tutamail.com>
2025-12-04 23:24:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import terms
+import Euclid
+import Pascal
+import perfection
+
+def main():
+    terms.main()
+    Euclid.main()
+    Pascal.main()
+    perfection.main()
+
+if __name__ == '__main__':
+    main()
