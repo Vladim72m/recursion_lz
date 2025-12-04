66227aa437d205d703711d4c425809e4f843a26c
Vladim72m <vladim72dx@tutamail.com>
2025-12-04 16:25:20 +0300

    Euclid.py

--- Euclid.py
+++ Euclid.py
@@ -2,7 +2,7 @@ def gcd(a,b):
     if b==0:
         return a
     else:
-        return gcd(b,a%b)
+        return gcd(b,a % b)
 def main():
     a=int(input('Введите первое число:'))
     b=int (input('Введите второе число:'))
@@ -10,3 +10,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
