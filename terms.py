a905b02fd614d610dd0e628ae2bff7ad5ca09514
Vladim72m <vladim72dx@tutamail.com>
2025-12-04 15:10:02 +0300

    Add files via upload

---
+++ terms.py
@@ -0,0 +1,11 @@
+print('Введите число')
+n = int(input())
+def operation(a, n, k):
+    if n:
+        for i in range(n+1, 0, -1): # -1 - длина шага 
+            if i <= k:
+                operation(a+[i], n-i, i)
+    else:
+        print( a, end=' , ')
+print('operation(',n,')=')  
+operation([], n, n)
